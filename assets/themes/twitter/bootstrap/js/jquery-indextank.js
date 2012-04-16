1bee6051acd23bfc2e1ec1b027a39e1d5f6b45a6
hopeww webmaster <joyce.chan@hopewwcanada.org>
2012-04-16 16:29:40 -0400

    lowered res

--- assets/themes/twitter/bootstrap/js/jquery-indextank.js
+++ assets/themes/twitter/bootstrap/js/jquery-indextank.js
@@ -22,45 +22,22 @@
       $('div#results').append("<br /></div><div class='modal-footer'><a href='' class='btn' data-dismiss='modal' >Close</a></div>");
     }
     function search(query) {
-      //var result = $.getJSON('http://hopewwc.api.houndsleuth.com/v1/indexes/HOPEWW/search?q=' + encodeURIComponent(query) + '&fetch=title&snippet=text&callback=?', function(data) {
-      //var result = $.getJSON('http://txje.api.indextank.com/v1/indexes/HOPE_WorldWide_of_Canada/search?q=' + encodeURIComponent(query) + '&fetch=title&snippet=text&callback=?', function(data) {
-      /*  
-      var result = $.getJSON('http://tapirgo.com/api/1/search.json?token=4f8586e03f61b06e0a00001c?query=' + encodeURIComponent(query) + '&callback=?', function(data) {
+      var url = 'http://tapirgo.com/api/1/search.json?token=4f8c429b3f61b01c2a000067&query=' + encodeURIComponent(query) + '&callback=?';
+      var result =  $.getJSON(url, function(data){
         create_heading();
-        var cnt = 0;
         var results_str = "";
-        index tank
-        $.each(data.results, function(index, result) {
-          results_str += '<div id="result"><br /><p><a href="' + result.docid + '">' + result.docid + '</a><br/>' + result.snippet_text + '</p></div>';
+        var cnt = 0;
+        $.each(data, function(key, val) {
+          results_str += '<div id="result"><p><a href="' + val.link + '">' + val.title + '</a><br />' + val.summary + '</p></div>'
           cnt +=1;
         });
-        if(cnt==0){
-          $('#results_body').append(" Found 0 results.");
-        }else{
-          $('#results_body').append(results_str);
-        }
+         if(cnt==0){
+            $('#results_body').append(" Found 0 results.");
+          }else{
+            $('#results_body').append(results_str);
+          }
         create_footer();
-        $("#results").modal({show:true});
-      } //end call back function 
-    ); //end var result
-  */
-
-    var url = 'http://tapirgo.com/api/1/search.json?token=4f8c429b3f61b01c2a000067&query=' + encodeURIComponent(query) + '&callback=?';
-    var result =  $.getJSON(url, function(data){
-      create_heading();
-      var results_str = "";
-      var cnt = 0;
-      $.each(data, function(key, val) {
-        results_str += '<div id="result"><p><a href="' + val.link + '">' + val.title + '</a><br />' + val.summary + '</p></div>'
-        cnt +=1;
-      });
-       if(cnt==0){
-          $('#results_body').append(" Found 0 results.");
-        }else{
-          $('#results_body').append(results_str);
-        }
-      create_footer();
-       $("#results").modal({show:true});
-      //alert (url);
-    }); //end call back & end var result
-  }
+         $("#results").modal({show:true});
+        //alert (url);
+      }); //end call back & end var result
+    }
